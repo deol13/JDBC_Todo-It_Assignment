626e16b3aa2b9188ac802cac604908ca185e6636
Dennis Olsen <dennan_lol@hotmail.com>
2025-05-16 16:08:08 +0200

    People interface implementation done

---
+++ src/main/java/se/lexicon/Data/Impl/PeopleImpl.java
@@ -0,0 +1,172 @@
+package se.lexicon.Data.Impl;
+
+import se.lexicon.Data.People;
+import se.lexicon.Model.Person;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class PeopleImpl implements People {
+    private Connection connection;
+
+    public PeopleImpl(Connection connection) { this.connection = connection; }
+
+    @Override
+    public Person create(Person person) {
+        String sql = "INSERT INTO person (first_name, last_name) VALUES(?,?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS) ) {
+
+            preparedStatement.setString(1, person.getFirst_name());
+            preparedStatement.setString(2, person.getLast_name());
+
+            int affectedRows = preparedStatement.executeUpdate();
+            System.out.println(affectedRows);
+
+            if (affectedRows > 0) {
+                try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                    if (resultSet.next()) {
+                        int generatedPersonId = resultSet.getInt(1);
+                        System.out.println("generatedPersonId = " + generatedPersonId);
+                        person.setPerson_id(generatedPersonId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error saving person");
+            e.printStackTrace();
+        }
+        return person;
+    }
+
+    @Override
+    public Collection<Person> findAll() {
+        List<Person> people = new ArrayList<>();
+
+        String sql = "SELECT * FROM person";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql) ) {
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int personId = resultSet.getInt("person_id");
+                    String first_name = resultSet.getString("first_name");
+                    String last_name = resultSet.getString("last_name");
+                    people.add(new Person(personId, first_name, last_name));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error finding all people");
+            e.printStackTrace();
+        }
+        return people;
+    }
+
+    @Override
+    public Person findById(int id) {
+        Person person = null;
+
+        String sql = "SELECT * FROM person WHERE person_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql) ) {
+
+            preparedStatement.setInt(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int personId = resultSet.getInt("person_id");
+                    String first_name = resultSet.getString("first_name");
+                    String last_name = resultSet.getString("last_name");
+                    person = new Person(personId, first_name, last_name);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error finding person with " + id + " id");
+            e.printStackTrace();
+        }
+
+        return person;
+    }
+
+    @Override
+    public Collection<Person> findByName(String name) throws IllegalArgumentException {
+        List<Person> people = new ArrayList<>();
+
+        String sql = "SELECT * FROM person WHERE first_name LIKE ? AND last_name LIKE ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql) ) {
+
+            String[] nameArr = name.split(" ");
+
+            if(nameArr.length != 2) throw new IllegalArgumentException("Error: Need first and last name!");
+            preparedStatement.setString(1, nameArr[0]);
+            preparedStatement.setString(2, nameArr[1]);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int personId = resultSet.getInt("person_id");
+                    String first_name = resultSet.getString("first_name");
+                    String last_name = resultSet.getString("last_name");
+                    people.add(new Person(personId, first_name, last_name));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error finding people with name " + name);
+            e.printStackTrace();
+        }
+        return people;
+    }
+
+    @Override
+    public Person update(Person person) {
+        //Person newPerson = person;
+        String sql = "UPDATE person SET first_name = ? , last_name = ? WHERE person_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql) ) {
+            preparedStatement.setString(1, person.getFirst_name());
+            preparedStatement.setString(2, person.getLast_name());
+            preparedStatement.setInt(3, person.getPerson_id());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("Update successful");
+//                try (ResultSet resultSet = preparedStatement.getResultSet()) {
+//                    while (resultSet.next()) {
+//                        int personId = resultSet.getInt("person_id");
+//                        String first_name = resultSet.getString("first_name");
+//                        String last_name = resultSet.getString("last_name");
+//                        newPerson = new Person(personId, first_name, last_name);
+//                    }
+//                }
+            }
+            else System.out.println("Update failed");
+
+        } catch (SQLException e) {
+            System.out.println("Error updating person");
+            e.printStackTrace();
+        }
+
+        return person;//newPerson;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String sql = "DELETE FROM person WHERE person_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql) ) {
+            preparedStatement.setInt(1, id);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error updating person");
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,169 @@
 package se.lexicon;
 
+import se.lexicon.DB.MySQLConnection;
+import se.lexicon.Data.Impl.PeopleImpl;
+import se.lexicon.Data.Impl.TodoItemsImpl;
+import se.lexicon.Model.Person;
+import se.lexicon.Model.TodoItem;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //personCreate();
+        //personFindAll();
+        //personFindById();
+        //personFindByName();
+        //personUpdate();
+        //personDelete();
+
+    }
+
+    public static void todoItemFindAll() {
+        try {
+            TodoItemsImpl items = new TodoItemsImpl(MySQLConnection.getConnection());
+
+            List<TodoItem> itemList = items.findAll().stream().toList();
+
+            itemList.forEach(i -> System.out.println("id: " + i.getTodo_id() + " , title: " + i.getTitle() + " , person_id: " + i.getAssignee_id()));
+            System.out.println("Operation is Done!");
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void todoItemFindById(){
+        try{
+            TodoItemsImpl items = new TodoItemsImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an id");
+            int id = scanner.nextInt();
+
+            TodoItem savedItem = items.findById(id);
+
+            System.out.println("savedPerson = " + savedItem.getTodo_id() + ": " +  savedItem.getTitle());
+            System.out.println("Operation is Done!");
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+
+
+    public static void personCreate() {
+        try{
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a first name:");
+            String fName = scanner.nextLine();
+            System.out.println("Enter a last name:");
+            String lName = scanner.nextLine();
+
+            Person savedPerson = people.create(new Person(fName, lName));
+
+            System.out.println("savedPerson = " + savedPerson.getFirst_name() + " " + savedPerson.getLast_name());
+            System.out.println("Operation is Done!");
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void personFindAll() {
+        try {
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            List<Person> personList = people.findAll().stream().toList();
+
+            personList.forEach(p -> System.out.println(p.getPerson_id() + ": " + p.getFirst_name() + " " + p.getLast_name()));
+            System.out.println("Operation is Done!");
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void personFindById(){
+        try{
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an id");
+            int pId = scanner.nextInt();
+
+            Person savedPerson = people.findById(pId);
+
+            System.out.println("savedPerson = " + savedPerson.getPerson_id() + ": " +  savedPerson.getFirst_name() + " " + savedPerson.getLast_name());
+            System.out.println("Operation is Done!");
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void personFindByName(){
+        try{
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a name");
+            String name = scanner.nextLine();
+
+            List<Person> personList = people.findByName(name).stream().toList();
+
+            personList.forEach(p -> System.out.println(p.getPerson_id() + ": " + p.getFirst_name() + " " + p.getLast_name()));
+            System.out.println("Operation is Done!");
+        } catch (IllegalArgumentException e){
+            System.out.println("Wrong amount of input, need a fist and last name with a space between them.");
+        }
+        catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void personUpdate(){
+        try{
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an id to change");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Enter a first name");
+            String fName = scanner.nextLine();
+            System.out.println("Enter a last name");
+            String lName= scanner.nextLine();
+
+            Person updatedPerson = people.update(new Person(id, fName, lName));
+
+            System.out.println("savedPerson = " + updatedPerson.getPerson_id() + ": " + updatedPerson.getFirst_name() + " " + updatedPerson.getLast_name());
+            System.out.println("Operation is Done!");
+        }
+        catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void personDelete(){
+        try{
+            PeopleImpl people = new PeopleImpl(MySQLConnection.getConnection());
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an id to change");
+            int id = scanner.nextInt();
+
+            if(people.delete(id)) System.out.println("Delete Success");
+            else System.out.println("Delete Failure");
+
+            System.out.println("Operation is Done!");
+        }
+        catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
     }
 }
